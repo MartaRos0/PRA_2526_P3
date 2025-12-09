ec88d855d4487c9fe1dff3b9fe254f38e4721f31
Marta Ros <marta@portatilMarta.localdomain>
2025-12-09 19:17:14 +0100

    Añadida implementación de la clase BSTree

--- BSTree.h
+++ BSTree.h
@@ -5,14 +5,10 @@
 #include <stdexcept>
 #include "BSNode.h"
 using namespace std;
-template <typename T> 
-class BSTree {
-    private:
 
-	    int nelem;
-	    BSNode<T>* root;
+template <typenam<T>* root;
 		
-		//Busqueda recursiva
+		
 	    BSNode<T>* search(BSNode<T>* n, T e) const{ 
 	    	if(n==nullptr){
 				throw runtime_error("Elemento no encontrado");
@@ -40,34 +36,74 @@ class BSTree {
 	    }
 
 	    void print_inorder(ostream &out, BSNode<T>* n) const{
-	    	if(n==nullptr){
+	    	if(n != nullptr){ 
 				print_inorder(out, n->left);   
 				out << n->elem << " ";        
 				print_inorder(out, n->right);  
 			}
 	    }
 
+		
 		T max(BSNode<T>* n) const {
-			if (n != nullptr) {
-				throw runtime_error("Elemento no encontrado");
+			if (n == nullptr) { 
+				throw runtime_error("El arbol esta vacio");
 			} else if (n->right != nullptr) {
 				return max(n->right);  
 			} else {
-				return n->elem;
+				return n->elem; 
 			}
     	}
 
+		
 		BSNode<T>* remove_max(BSNode<T>* n){
-
-			//aun no está acabada
+			if (n->right == nullptr) { // Si no hay hijo derecho, este es el nodo máximo
+				BSNode<T>* temp = n->left; // Guarda el subárbol izquierdo (puede ser nullptr)
+				delete n; // Borra el nodo actual (el máximo)
+				nelem--;
+				return temp; // El nodo padre se conectará a 'temp'
+			} else {
+				n->right = remove_max(n->right); // Continúa buscando el máximo a la derecha
+				return n;
+			}
 		}
 
+		// Método principal para eliminar un elemento
 		BSNode<T>* remove(BSNode<T>* n, T e) {
-			//aun no está acabada
+			if (n == nullptr) {
+				throw runtime_error("Elemento no encontrado para eliminar");
+			} else if (e < n->elem) { // Busca a la izquierda
+				n->left = remove(n->left, e);
+			} else if (e > n->elem) { // Busca a la derecha
+				n->right = remove(n->right, e);
+			} else { // n->elem == e: Elemento encontrado
+				
+				if (n->right == nullptr) {
+					BSNode<T>* temp = n->left;
+					delete n;
+					nelem--;
+					return temp;
+				}
+				
+				else if (n->left == nullptr) {
+					BSNode<T>* temp = n->right;
+					delete n;
+					nelem--;
+					return temp;
+				}
+				
+				else {
+					n->elem = max(n->left);
+					n->left = remove(n->left, n->elem);
+				}
+			}
+			return n;
 		}
-
 		void delete_cascade(BSNode<T>* n) {
-			//aun no está acabada
+			if (n != nullptr) {
+				delete_cascade(n->left);
+				delete_cascade(n->right);
+				delete n; 
+			}
 		}
     public:
 	    BSTree(){
@@ -97,7 +133,7 @@ class BSTree {
 		}
 
 		~BSTree() {
-			//aun no está acabada
+			delete_cascade(root);
     	}
 
 	    friend std::ostream& operator<<(std::ostream &out, const BSTree<T> &bst){
