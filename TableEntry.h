9cabe73d6b5af015254e041f94e69a6fe1068a3a
Marta Ros <marta@portatilMarta.localdomain>
2025-12-12 19:05:08 +0100

    Implementada la sobrecarga de operadores de comparación en TableEntry para BSTreeDict

--- TableEntry.h
+++ TableEntry.h
@@ -3,36 +3,45 @@
 
 #include <string>
 #include <ostream>
+#include <stdexcept>
 using namespace std;
+
 template <typename V> 
 class TableEntry {
-    public:
+public:
+    string key;
+    V value;
 
-	    string key;
-	    V value;
+    // Constructores
+    TableEntry(string k, V v) : key(k), value(v) {}
+    TableEntry(const TableEntry<V>& other) : key(other.key), value(other.value) {}
+    TableEntry(string k) : key(k) {} // Constructor útil para la búsqueda (solo con clave)
 
-	    TableEntry(string _key, V _value){
-	    	key=_key;
-		value=_value;
-	    }
-	    TableEntry(string n_key){
-	    	key=n_key;
-	    }
-	    TableEntry(){
-	    	key=""; 
-	    }
-	    friend bool operator==(const TableEntry<V> &te1, const TableEntry<V> &te2){
-	    	return (te1.key == te2.key);
-	    }
-	    friend bool operator!=(const TableEntry<V> &te1, const TableEntry<V> &te2){
-	    	return (te1.key != te2.key);
-	    }
-	    friend std::ostream& operator<<(std::ostream &out, const TableEntry<V> &te){
-	    	out << "('" << te.key << "' => " << te.value << ")";
-		return out;
-	    }
+    // Sobrecarga de operadores de comparación (< y >) por clave.
+    // Necesarios para la navegación en BSTree.
+    friend bool operator<(const TableEntry<V> &te1, const TableEntry<V> &te2) {
+        return te1.key < te2.key; 
+    }
 
+    friend bool operator>(const TableEntry<V> &te1, const TableEntry<V> &te2) {
+        return te1.key > te2.key;
+    }
     
+    // El BSTree.h utiliza '==' en el método search
+    friend bool operator==(const TableEntry<V> &te1, const TableEntry<V> &te2) {
+        return te1.key == te2.key;
+    }
+    
+    // El BSTree.h utiliza '!=' en el método search
+    friend bool operator!=(const TableEntry<V> &te1, const TableEntry<V> &te2) {
+        return te1.key != te2.key;
+    }
+
+    // Sobrecarga del operador de impresión
+    friend std::ostream& operator<<(std::ostream &out, const TableEntry<V> &te){
+        out << "('" << te.key << "' => " << te.value << ")";
+        return out;
+    }
 };
 
 #endif
