6fa48421a36d2b7e6a4286f2bd195b59db176211
marta <mrostel1@upv.edu.es>
2025-12-16 10:50:27 +0100

    Añadido operadores de comparación

--- TableEntry.h
+++ TableEntry.h
@@ -29,8 +29,15 @@ class TableEntry {
 	    }
 	    friend std::ostream& operator<<(std::ostream &out, const TableEntry<V> &te){
 	    	out << "('" << te.key << "' => " << te.value << ")";
-		return out;
+			return out;
 	    }
+		friend bool operator<(const TableEntry<V> &te1, const TableEntry<V> &te2) {
+		    return te1.key < te2.key; 
+		}
+		
+		friend bool operator>(const TableEntry<V> &te1, const TableEntry<V> &te2) {
+		    return te1.key > te2.key;
+		}
 
     
 };
